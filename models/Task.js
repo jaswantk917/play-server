44159fd8b5ac764d97ce996c7d4b0849b49ffb73
Jaswant Kumar <jaswantk917@gmail.com>
2026-01-07 12:39:25 +0530

    schema

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  isCompleted: { type: Boolean, default: false },
-});
+const TaskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    isCompleted: { type: Boolean, default: false },
+  },
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  },
+);
 
 module.exports = mongoose.model("Task", TaskSchema);
